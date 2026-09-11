d41effcf7bb38f205a620fbf187ceec2903a8e2b
Christopher Lawler <christopher.r.lawler@jpl.nasa.gov>
2026-09-11 16:01:27 -0700

    adding more unit tests for engine exception handling inside activities - trying to find potential hang, so far all tests work

--- src/test/java/gov/nasa/jpl/engine/ModelingEngineTest.java
+++ src/test/java/gov/nasa/jpl/engine/ModelingEngineTest.java
@@ -3,12 +3,19 @@ package gov.nasa.jpl.engine;
 import gov.nasa.jpl.activity.Activity;
 import gov.nasa.jpl.activity.ActivityInstanceList;
 import gov.nasa.jpl.common.BaseTest;
+import gov.nasa.jpl.exampleAdaptation.ActivityOne;
+import gov.nasa.jpl.exampleAdaptation.ActivityTwo;
+import gov.nasa.jpl.exampleAdaptation.Res;
 import gov.nasa.jpl.resource.IntegerResource;
 import gov.nasa.jpl.resource.ResourceList;
 import gov.nasa.jpl.time.Duration;
 import gov.nasa.jpl.time.Time;
 import org.junit.Before;
 import org.junit.Test;
+import spice.basic.CSPICE;
+import spice.basic.SpiceErrorException;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -22,7 +29,7 @@ public class ModelingEngineTest extends BaseTest {
     @Test
     public void successfullyCatchBrokenActivity(){
         FailedActivity act = new FailedActivity(Time.getDefaultReferenceTime());
-        BadActivity act2 = new BadActivity(Time.getDefaultReferenceTime().add(Duration.MINUTE_DURATION));
+        FailedActivity actDos = new FailedActivity(Time.getDefaultReferenceTime());
 
         try{
             ModelingEngine.getEngine().model();
@@ -31,6 +38,45 @@ public class ModelingEngineTest extends BaseTest {
         catch (RuntimeException ex){
             assertTrue(ex.getMessage().contains("Catch this"));
         }
+
+        ActivityInstanceList.getActivityList().clear();
+        for(int i = 0; i<100; i++) {
+            ActivityOne workingAct = new ActivityOne(Time.getDefaultReferenceTime(), Duration.SECOND_DURATION);
+        }
+
+        BadActivity act2 = new BadActivity(Time.getDefaultReferenceTime().add(new Duration("00:00:30")));
+        try{
+            ModelingEngine.getEngine().model();
+            fail("Should not have proceeded past runtime exception");
+        }
+        catch (RuntimeException ex){
+            assertTrue(ex.getMessage().contains("/ by zero"));
+        }
+
+        ActivityInstanceList.getActivityList().clear();
+        for(int i = 0; i<100; i++) {
+            ActivityTwo workingAct = new ActivityTwo(Time.getDefaultReferenceTime(), 5.0);
+        }
+        BrokenFuncActivity act3 = new BrokenFuncActivity(Time.getDefaultReferenceTime().add(Duration.MINUTE_DURATION));
+        BrokenFuncActivity act4 = new BrokenFuncActivity(Time.getDefaultReferenceTime().add(Duration.MINUTE_DURATION));
+        try{
+            ModelingEngine.getEngine().model();
+            fail("Should not have proceeded past runtime exception");
+        }
+        catch (RuntimeException ex){
+            assertTrue(ex.getMessage().contains("oops"));
+        }
+
+        ActivityInstanceList.getActivityList().clear();
+        for(Time t = Time.getDefaultReferenceTime(); t.lessThan(Time.getDefaultReferenceTime().add(Duration.DAY_DURATION)); t = t.add(Duration.MINUTE_DURATION)) {
+            CaughtRuntimeExceptionAct caughtAct = new CaughtRuntimeExceptionAct(t);
+            SwallowingCheckedErrorActivity actErr = new SwallowingCheckedErrorActivity(t);
+        }
+        ActivityOne workingAct = new ActivityOne(Time.getDefaultReferenceTime().add(Duration.HOUR_DURATION), Duration.SECOND_DURATION);
+        ModelingEngine.getEngine().model();
+        assertEquals(3, Res.ResourceA.getSize());
+
+        ActivityInstanceList.getActivityList().clear();
     }
 
     public static class FailedActivity extends Activity{
@@ -49,9 +95,62 @@ public class ModelingEngineTest extends BaseTest {
         }
 
         public void model() {
+            waitFor(new Duration("00:05:00"));
+            waitFor(new Duration("00:00:15"));
             int x = 1/0;
+            waitFor(new Duration("00:05:00"));
+            waitFor(new Duration("00:00:05"));
         }
     }
+
+    public static class BrokenFuncActivity extends Activity{
+        public BrokenFuncActivity(Time t){
+            super(t);
+        }
+
+        public Waiter modelFunc(){
+            throw new NullPointerException("oops");
+        }
+    }
+
+    public static class CaughtRuntimeExceptionAct extends Activity{
+        public CaughtRuntimeExceptionAct(Time t){
+            super(t);
+        }
+
+        public void model(){
+            waitFor(new Duration("00:05:00"));
+            waitFor(new Duration("00:00:15"));
+            try{
+                throw new AdaptationException("oops");
+            }
+            catch(AdaptationException e){
+                // do nothing
+            }
+            waitFor(new Duration("00:05:00"));
+            waitFor(new Duration("00:00:15"));
+        }
+    }
+
+    public static class SwallowingCheckedErrorActivity extends Activity{
+        public SwallowingCheckedErrorActivity(Time t){
+            super(t);
+        }
+
+        public void model(){
+            waitFor(new Duration("00:05:00"));
+            waitFor(new Duration("00:05:00"));
+            try{
+                CSPICE.spkezr("test", 0.0, "test", "test", "test", new double[0], new double[0]);
+            }
+            catch(SpiceErrorException e){
+                return;
+            }
+            waitFor(new Duration("00:10:00"));
+            waitFor(new Duration("00:05:00"));
+        }
+    }
+
     public static class ActivityWithTimeLoop extends Activity {
         private final IntegerResource res;
         /**
