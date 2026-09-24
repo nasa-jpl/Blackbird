b1239f5d9315a7c878161bb2ba2fdcf154bcff94
Christopher Lawler <christopher.r.lawler@jpl.nasa.gov>
2026-09-23 19:05:33 -0700

    exposed issue in parallel IO writer resource thread where it uses iterator that can have past nodes in it - fix is in include_activities_bridging_start_time_writeout but I may need to bring that code into here

--- src/test/java/gov/nasa/jpl/input/ParallelIOTest.java
+++ src/test/java/gov/nasa/jpl/input/ParallelIOTest.java
@@ -10,10 +10,14 @@ import gov.nasa.jpl.time.Time;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static gov.nasa.jpl.input.XMLTOLHistoryReaderTest.readInHistoryOfActivitiesAndResource;
-import static org.junit.Assert.fail;
+import static gov.nasa.jpl.output.tol.XMLTOLWriterTest.createSimulationAndWriteOutFile;
+import static org.junit.Assert.*;
 
 public class ParallelIOTest extends BaseTest {
     @Before
@@ -55,6 +59,37 @@ public class ParallelIOTest extends BaseTest {
 
     @Test
     public void testIncludingInconResources(){
-        fail("test");
+        createSimulationAndWriteOutFile("history_unit_test_resource_incon.dir", true);
+
+        BufferedReader br = null;
+        List<List<String>> contents = new ArrayList<>();
+        try{
+            br = new BufferedReader(new FileReader("history_unit_test_resource_incon.dir" + File.separator + "resources" + File.separator + "PositionVector[y].csv"));
+            String line = br.readLine();
+            while (line != null) {
+                contents.add(Arrays.asList(line.split(",")));
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // check that the first entry corresponds to the desired incon time
+        Time priorTime = null;
+        for(int i = 0; i< contents.size(); i++){
+            // check that the first entry is the incon value we want
+            if(i == 0){
+                assertEquals("2000-002T00:02:20.000000", contents.get(i).get(0));
+                assertEquals("0.02", contents.get(i).get(2));
+                priorTime = new Time(contents.get(i).get(0));
+            }
+            // check that all the entries following in the output are in time order
+            else{
+                Time currentTime = new Time(contents.get(i).get(0));
+                assertTrue(currentTime.greaterThan(priorTime));
+                priorTime = currentTime;
+            }
+        }
     }
 }
